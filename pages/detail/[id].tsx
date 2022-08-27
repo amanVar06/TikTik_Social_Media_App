dc997457fde28ef643ca6195d578ee25a7050c65
amanVar06 <98205162+amanVar06@users.noreply.github.com>
2022-08-27 21:50:46 +0530

    Added like functionality to the post but doesn't seems to be working right will fix it later

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 import { GoVerified } from "react-icons/go";
 import { MdOutlineCancel } from "react-icons/md";
 import { BsFillPlayFill } from "react-icons/bs";
 import { HiVolumeOff, HiVolumeUp } from "react-icons/hi";
+import axios from "axios";
 import { Video } from "../../types";
 
-import axios from "axios";
+import { BASE_URL } from "../../utils";
+import useAuthStore from "../../store/authStore";
+
+import LikeButton from "../../components/LikeButton";
+import Comments from "../../components/Comments";
 
 interface Iprops {
   postDetails: Video;
@@ -20,6 +25,8 @@ const Detail = ({ postDetails }: Iprops) => {
   const [playing, setPlaying] = useState(false);
   const [isVideoMuted, setIsVideoMuted] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
+  const router = useRouter();
+  const { userProfile }: any = useAuthStore();
 
   const onVideoClick = () => {
     if (playing) {
@@ -41,20 +48,33 @@ const Detail = ({ postDetails }: Iprops) => {
     }
   }, [post, isVideoMuted]);
 
+  const handleLike = async (like: boolean) => {
+    if (userProfile) {
+      //if we want to update something we will a put request
+      const { data } = await axios.put("http://localhost:3000/api/like", {
+        userId: userProfile._id,
+        postId: post._id,
+        like,
+      });
+
+      setPost({ ...post, likes: data.likes });
+    }
+  };
+
   if (!post) return null;
 
   return (
     <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
       <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center">
         <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
-          <p>
+          <p className="cursor-pointer" onClick={() => router.back()}>
             <MdOutlineCancel className="text-white text-[35px]" />
           </p>
         </div>
 
         {/* setting up video view now  */}
         <div className="relative">
-          <div className="lg:h-[100vh] h-[60vh]">
+          <div className="lg:h-[100vh] h-[80vh]">
             <video
               ref={videoRef}
               loop
@@ -83,6 +103,59 @@ const Detail = ({ postDetails }: Iprops) => {
               <HiVolumeUp className="text-white text-2xl lg:text-4xl" />
             </button>
           )}
+        </div>
+      </div>
+
+      <div className="relative w-[1000px] md:w-[900px] lg:w-[700px]">
+        <div className="lg:mt-20 mt-10">
+          {/*copied frm video card*/}
+          <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+            <div className="ml-4 md:w-20 md:h-20 w-16 h-16">
+              <Link href="/">
+                {/* you cant put an image as a child componentof link  */}
+                <>
+                  <Image
+                    width={62}
+                    height={62}
+                    className="rounded-full"
+                    src={post.postedBy.image}
+                    alt="profile photo"
+                    layout="responsive"
+                  />
+                </>
+              </Link>
+            </div>
+            <div>
+              <Link href="/">
+                <div className="mt-3 flex flex-col gap-2">
+                  {/* this div for the username and the actual name  */}
+                  <p className="flex gap-2 items-center lg:text-lg md:text-md font-bold text-primary">
+                    {post.postedBy.userName} {` `}
+                    <GoVerified className="text-blue-400 text-md" />
+                  </p>
+
+                  <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                    {post.postedBy.userName}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          </div>
+
+          <p className="px-10 text-lg text-gray-600">{post.caption}</p>
+
+          <div className="mt-10 px-10">
+            {/* we can only like if we are logged in  */}
+            {userProfile && (
+              <LikeButton
+                likes={post.likes}
+                handleLike={() => handleLike(true)}
+                handleDislike={() => handleLike(false)}
+              />
+            )}
+          </div>
+
+          <Comments />
         </div>
       </div>
     </div>
